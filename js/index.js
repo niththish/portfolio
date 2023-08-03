b003f2cc86bca742587eaddc06710a2ae4513226
niththish <niththish21@gmail.com>
2023-08-03 11:22:41 +0530

    animated image in home-content

--- js/index.js
+++ js/index.js
@@ -12,3 +12,11 @@ toggleBtn.addEventListener("click", () => {
   const navLinks = document.getElementsByClassName("nav-links")[0];
   navLinks.classList.toggle("toggle-view");
 });
+
+let image = document.querySelector(".right-content .image");
+let home = document.querySelector("#home");
+home.addEventListener("mousemove", function (e) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  image.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
